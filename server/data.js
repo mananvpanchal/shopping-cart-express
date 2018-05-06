a24ecb13390fe8fc84961df433515c1d329f984b
Manan <mpanchal@integconsulting.com>
2018-05-06 18:50:14 +0530

    done

--- server/data.js
+++ server/data.js
@@ -1,26 +1,162 @@
 module.exports = [
     {
+        id: '1',
         company: 'LG',
         product: 'Television',
-        modal: 'HQL234',
-        price: '12000'
+        model: 'HQL234',
+        price: '12000',
+        count: 10
     },
     {
+        id: '2',
         company: 'Samsung',
         product: 'Television',
-        modal: 'HJH354',
-        price: '15000'
+        model: 'HJH354',
+        price: '15000',
+        count: 10
     },
     {
+        id: '3',
         company: 'Sony',
         product: 'Television',
-        modal: 'KJH873',
-        price: '13000'
+        model: 'KJH873',
+        price: '13000',
+        count: 10
     },
     {
+        id: '4',
         company: 'Penasonic',
         product: 'Television',
-        modal: 'YGE237',
-        price: '14000'
+        model: 'YGE237',
+        price: '14000',
+        count: 10
+    },
+    {
+        id: '5',
+        company: 'Micromax',
+        product: 'Television',
+        model: 'YWE962',
+        price: '15000',
+        count: 10
+    },
+    {
+        id: '6',
+        company: 'LG',
+        product: 'Phone',
+        model: 'HQL234',
+        price: '12000',
+        count: 10
+    },
+    {
+        id: '7',
+        company: 'Samsung',
+        product: 'Phone',
+        model: 'HJH354',
+        price: '15000',
+        count: 10
+    },
+    {
+        id: '8',
+        company: 'Sony',
+        product: 'Phone',
+        model: 'KJH873',
+        price: '13000',
+        count: 10
+    },
+    {
+        id: '9',
+        company: 'Penasonic',
+        product: 'Phone',
+        model: 'YGE237',
+        price: '14000',
+        count: 10
+    },
+    {
+        id: '10',
+        company: 'Micromax',
+        product: 'Phone',
+        model: 'YWE962',
+        price: '15000',
+        count: 10
+    },
+    {
+        id: '11',
+        company: 'LG',
+        product: 'Tablet',
+        model: 'HQL234',
+        price: '12000',
+        count: 10
+    },
+    {
+        id: '12',
+        company: 'Samsung',
+        product: 'Tablet',
+        model: 'HJH354',
+        price: '15000',
+        count: 10
+    },
+    {
+        id: '13',
+        company: 'Sony',
+        product: 'Tablet',
+        model: 'KJH873',
+        price: '13000',
+        count: 10
+    },
+    {
+        id: '14',
+        company: 'Penasonic',
+        product: 'Tablet',
+        model: 'YGE237',
+        price: '14000',
+        count: 10
+    },
+    {
+        id: '15',
+        company: 'Micromax',
+        product: 'Tablet',
+        model: 'YWE962',
+        price: '15000',
+        count: 10
+    },
+    {
+        id: '16',
+        company: 'LG',
+        product: 'Refrigerator',
+        model: 'HQL234',
+        price: '12000',
+        count: 10
+    },
+    {
+        id: '17',
+        company: 'Samsung',
+        product: 'Refrigerator',
+        model: 'HJH354',
+        price: '15000',
+        count: 10
+    },
+    {
+        id: '18',
+        company: 'Videocon',
+        product: 'Refrigerator',
+        model: 'KJH873',
+        price: '13000',
+        count: 10
+    },
+    {
+        id: '19',
+        company: 'Godrej',
+        product: 'Refrigerator',
+        model: 'YGE237',
+        price: '14000',
+        count: 10
+    },
+    {
+        id: '20',
+        company: 'Haier',
+        product: 'Refrigerator',
+        model: 'YWE962',
+        price: '15000',
+        count: 10
     }
 ];
--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ const app = express();
 
 app.use(cors());
 
-app.get('/list', (req, res) => res.send(data));
+app.get('/list', (req, res) => {
+    const chunk = data.slice(req.query.start, req.query.end);
+    return res.send(chunk); 
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
