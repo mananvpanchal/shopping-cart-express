66c15e979d0c120e4a72e0a32aabeed55d77581d
Manan <mpanchal@integconsulting.com>
2018-05-10 01:07:42 +0530

    infinite data

--- server/server.js
+++ server/server.js
@@ -8,8 +8,17 @@ const app = express();
 app.use(cors());
 
 app.get('/list', (req, res) => {
-    const chunk = data.slice(req.query.start, req.query.end);
-    return res.send(chunk); 
+    if(data.length >= req.query.end) {
+        const chunk = data.slice(req.query.start, req.query.end);
+        return res.send(chunk); 
+    } else {
+        const chunk = [];
+        for(let i = parseInt(req.query.start); i < parseInt(req.query.end); i++) {
+            chunk.push({ id: ''+(i+1), company: 'Company '+(i+1), 
+            product: 'Product '+(i+1), model :'Model '+(i+1), price: i+1, count: 10});
+        }
+        return res.send(chunk); 
+    }
 });
 
 const port = process.env.PORT || 5000;
